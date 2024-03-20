0ef894b69c2c1f6f942ab830b3be343d7c20e876
Timothy Barasa <timbaros87@gmail.com>
2024-03-20 10:16:58 +0300

    Adds arithmetic operators and parsing strings

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+// console.log(add(606, 606));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+console.log(increment(3))
+
+function decrement(n) {
+    return n - 1;
+}
+console.log(decrement(10))
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
